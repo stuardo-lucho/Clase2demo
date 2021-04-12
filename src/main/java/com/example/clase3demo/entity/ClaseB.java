6c8d466a61f4e0cea4ffdbf42f81ce58caeb8062
Stuardo Lucho Hp Omen <stuardo.lucho@pucp.edu.pe>
2021-04-12 17:39:35 -0500

    primeros cambios

--- src/main/java/com/example/clase3demo/entity/ClaseB.java
+++ src/main/java/com/example/clase3demo/entity/ClaseB.java
@@ -1,4 +1,43 @@
 package com.example.clase3demo.entity;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class ClaseB {
+
+    //nuevo comentario
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int shipperid;
+    @Column(nullable = false)
+    private String companyname;
+    private String phone;
+
+    public int getShipperid() {
+        return shipperid;
+    }
+
+    public void setShipperid(int shipperid) {
+        this.shipperid = shipperid;
+    }
+
+    public String getCompanyname() {
+        return companyname;
+    }
+
+    public void setCompanyname(String companyname) {
+        this.companyname = companyname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
 }
